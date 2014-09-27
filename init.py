b0c41018088028fcfcc5d5e9916549670b491292
Carmine Paolino <carmine@paolino.me>
2014-09-27 17:35:36 +0200

    Init procedures

---
+++ init.py
@@ -0,0 +1,16 @@
+import logging
+import config
+
+log_levels = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL
+}
+
+
+def init_logger():
+    conf = config.get_config()
+    log_level = conf.get('Output', 'LogLevel')
+    logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=log_levels[log_level])
