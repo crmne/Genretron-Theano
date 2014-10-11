767482ed1923de09b18fbfdd7db3464d062fcf32
Carmine Paolino <carmine@paolino.me>
2014-10-11 18:37:18 +0200

    Put target vector in h5 file (2/2)

--- track.py
+++ track.py
@@ -4,19 +4,22 @@ from spectrogram import Spectrogram
 
 conf = config.get_config()
 winsize = int(conf.get('Spectrogram', 'WindowSize'))
-nframes = int(conf.get('Tracks', 'LengthInSeconds')) * int(conf.get('Tracks', 'SampleRate'))
+nframes = int(conf.get('Tracks', 'LengthInSeconds')) * \
+    int(conf.get('Tracks', 'SampleRate'))
 stepsize = int(conf.get('Spectrogram', 'StepSize'))
 fftres = int(conf.get('Spectrogram', 'FFTResolution'))
+numberofgenres = int(conf.get('Tracks', 'NumberOfGenres'))
 wins = Spectrogram.wins(winsize, nframes, stepsize)
 bins = Spectrogram.bins(fftres)
 shape = Spectrogram.shape(wins, bins)
 
 
 class Track(IsDescription):
+
     """Description of a track in HDF5"""
     idnumber = Int64Col()
     name = StringCol(64)
     path = StringCol(512)
     genre = StringCol(32)
-    # TODO: get those value from somewhere else
+    target = BoolCol(shape=(numberofgenres,))
     spectrogram = Float32Col(dflt=0.0, shape=shape)
