b8c5aca4ff6649fbb87b330617e1b55012bd79e9
Carmine Paolino <carmine@paolino.me>
2014-10-11 18:36:00 +0200

    No need to print config with INI files

--- scripts/print_config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from os import sys, path
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-import config
-
-if __name__ == '__main__':
-    config.print_config()
